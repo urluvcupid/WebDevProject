d6a02e7eddfe5930dc3a0bd07d2f9b63b562a1fb
Foresi9ht <staya.ayanbek@gmail.com>
2025-04-13 15:30:38 +0500

    Добавил компонент Header и прописал все стили заранее!!! Пока фотки не прогружаются, но это все решим

--- Kinomonster/src/app/app.component.ts
+++ Kinomonster/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [HeaderComponent], // ← добавил сюда header
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Kinomonster';
-}
+export class AppComponent {}
