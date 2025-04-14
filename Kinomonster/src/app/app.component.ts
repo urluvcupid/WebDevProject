533985a7b9ad4418d023a6b8f510ffdeb1b8fab0
Dilshat <rakhmatulindilshat@gmail.com>
2025-04-14 23:05:11 +0500

    Добавил футер, добавил Temporary main, чтобы футер привязался к низу. Проблему с загрузкой фото потом решу

--- Kinomonster/src/app/app.component.ts
+++ Kinomonster/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent], // ← добавил сюда header
+  imports: [HeaderComponent, FooterComponent], // ← добавил сюда header //14.04.2025 UPD: добавил сюда footer
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
