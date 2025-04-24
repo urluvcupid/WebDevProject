ba3bff776669867f74fb3e64135f565185076478
Foresi9ht <staya.ayanbek@gmail.com>
2025-04-25 04:57:46 +0500

    Я создал django project в  отдельной папке бэкэнд. Я законектил их с нашим фронтом и добавил путь в app.component.ts!!!

--- Kinomonster/src/app/app.component.ts
+++ Kinomonster/src/app/app.component.ts
@@ -3,12 +3,32 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, RouterModule, FooterComponent], // ← добавил сюда header //14.04.2025 UPD: добавил сюда footer
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    RouterModule,
+    FooterComponent,
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  private http = inject(HttpClient);
+  message = '';
+
+  constructor() {
+    this.http
+      .get<{ message: string }>('http://localhost:8000/api/hello/')
+      .subscribe({
+        next: (res) => (this.message = res.message),
+        error: (err) => console.error('Ошибка запроса:', err),
+      });
+  }
+}
