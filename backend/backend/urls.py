2537357f751d97ff42db0a020caf0c1a7edec743
urluvcupid <el.talgatow@gmail.com>
2025-04-26 13:11:24 +0500

    some changes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('app.urls')),  # вот это подключает мои маршруты из app/urls.py
+    path('app/', include('app.urls')),  # вот это подключает мои маршруты из app/urls.py
 ]
